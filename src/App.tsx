09db8942486fab0a4d94f407459f2c4eeb8b8ca4
Pier2208 <p.leroy.mtl@gmail.com>
2023-11-30 12:51:42 -0500

    fix/improving component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import { DollarSignIcon, LogOut, User } from "lucide-react"
-import Dropdown from "./components/Dropdown"
+import { CircleEllipsis, DollarSignIcon, Edit, LogOut, PlusCircle, Trash, User, View } from "lucide-react"
+import {Dropdown, Trigger, MenuList, MenuItem} from "./components/Dropdown"
 
 
 function App() {
   return (
-   <main className="bg-sky-800 w-full min-h-screen flex items-center justify-center">
-     <Dropdown>
-      <Dropdown.Trigger />
-      <Dropdown.MenuList>
-        <Dropdown.MenuItem icon={User}>
-          Username
-        </Dropdown.MenuItem>
-        <Dropdown.MenuItem icon={DollarSignIcon}>
-          My account
-        </Dropdown.MenuItem>
-        <Dropdown.MenuItem icon={LogOut}>
-          Logout
-        </Dropdown.MenuItem>
-      </Dropdown.MenuList>
+   <main className="bg-sky-800">
+    <div className="flex min-h-screen items-center justify-center gap-x-48">
+    <Dropdown>
+      <Trigger icon={CircleEllipsis} />
+      <MenuList>
+        <MenuItem icon={Edit} name='Edit' />
+        <MenuItem icon={Trash} name="Trash" />
+        <MenuItem icon={View} name="View" />
+      </MenuList>
      </Dropdown>
+
+     <Dropdown>
+      <Trigger icon={PlusCircle} />
+      <MenuList>
+        <MenuItem icon={User} name='Username' />
+        <MenuItem icon={DollarSignIcon} name="My account" />
+        <MenuItem icon={LogOut} name="Logout" />
+      </MenuList>
+     </Dropdown>
+    </div>
    </main>
   )
 }
--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,71 +1,59 @@
-import { CircleEllipsis, LucideIcon } from "lucide-react";
+import { LucideIcon } from "lucide-react";
 import { createContext, useContext, useState } from "react";
-import { useClickOutside } from "../hooks/useClickOutside";
 
 interface DropdownContextType {
-  setDropdownOpen: React.Dispatch<React.SetStateAction<boolean>>,
-  dropdownOpen: boolean
+  toggle: () => void,
+  isOpen: boolean
 }
 
 const DropdownContext = createContext<DropdownContextType | null>(null)
 
-// gestionnaire de dropdowns
-export function DropdownProvider({ children }: { children: React.ReactNode}) {
-  const [dropdownOpen, setDropdownOpen] = useState(false)
+export function Dropdown({ children }: { children: React.ReactNode}) {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+
 
   return (
-    <DropdownContext.Provider value={{ dropdownOpen, setDropdownOpen}}>
-     {children}
+    <DropdownContext.Provider value={{ isOpen, toggle}}>
+      <div className="relative">
+        {children}
+      </div>
     </DropdownContext.Provider>
   )
 }
 
-export function Dropdown({ children }: { children: React.ReactNode}) {
+export function Trigger({ icon: Icon }: { icon: LucideIcon}) {
+  const {toggle, isOpen} = useContext(DropdownContext)!;
+
+  const handleClick = () => toggle()
+
   return (
-    <div className="relative">
-      {children}
+    <div onClick={handleClick}>
+      <Icon className={`h-8 w-8 text-white cursor-pointer ${isOpen ? 'text-slate-300' : 'text-white' }`} />
     </div>
   )
 }
 
-function Trigger() {
-  const {setDropdownOpen, dropdownOpen} = useContext(DropdownContext)!;
-  const ref = useClickOutside(() => setDropdownOpen(false))
+export function MenuList ({ children }: { children: React.ReactNode }) {
+  const {isOpen } = useContext(DropdownContext)!
 
-  const handleClick = () => {
-    setDropdownOpen(prev => !prev)
-  }
-
-  return (
-    <div ref={ref} onClick={handleClick}>
-      <CircleEllipsis className={`h-8 w-8 text-white cursor-pointer ${dropdownOpen ? 'text-slate-300' : 'text-white' }`} />
-    </div>
-  )
-}
-
-function MenuList ({ children }: { children: React.ReactNode }) {
-  const {dropdownOpen } = useContext(DropdownContext)!
-
-  return dropdownOpen ? (
-    <ul className="p-3 shadow-md bg-slate-100 absolute right-0 mt-2 rounded w-max flex flex-col gap-y-3">
+  return isOpen ? (
+    <ul className="shadow-md bg-slate-100 absolute right-0 mt-2 rounded w-max flex flex-col">
        {children}
     </ul>
   ) : null
 }
 
-function MenuItem({ children, icon: Icon }: { children: React.ReactNode, icon: LucideIcon}) {
-  const {setDropdownOpen} = useContext(DropdownContext)!
+export function MenuItem({ name, icon: Icon }: { name: string, icon: LucideIcon}) {
+  const {toggle} = useContext(DropdownContext)!
 
   return (
-    <li onClick={() => setDropdownOpen(false)} className="flex justify-between w-full text-sm hover:bg-slate-400 cursor-pointer">
-      <Icon className="h-4 w-4 mr-4" />
-      {children}
+    <li onClick={toggle} className="hover:bg-slate-200 cursor-pointer">
+      <div className="px-4 py-3 flex justify-between w-full text-sm">
+        <Icon className="h-4 w-4 mr-4" />
+        {name}
+      </div>
     </li>
   )
 }
 
-Dropdown.Trigger = Trigger
-Dropdown.MenuList = MenuList
-Dropdown.MenuItem = MenuItem
-
-export default Dropdown
